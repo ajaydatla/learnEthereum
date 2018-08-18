2c9f050fed3f03932fd59a81d124c9150cbbf25a
ajaydatla <ajaydatlalbn06@gmail.com>
2018-08-18 17:49:14 +0530

    compo

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+
+const api_key = 'AIzaSyAuQCVeNfKhRk9klChQPT1n027DP0_5Ss';
+YTSearch({key: api_key, term:'surfboards'}, function(data){
+    console.log(data);
+});
 
 
 const App = () => {
