891a270a641dff861333d935ddad461151de06c3
ajaydatla <ajaydatlalbn06@gmail.com>
2018-08-20 15:38:42 +0530

    lottery contract finished

--- lottery/deploy.js
+++ lottery/deploy.js
@@ -2,6 +2,7 @@ const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
+
 const provider = new HDWalletProvider(
     'thumb prefer scissors busy box tide adjust runway odor lesson rather tent',
     'https://rinkeby.infura.io/v3/e87a5d84da6b41e8a670e5e5d7c0923f'
@@ -14,9 +15,9 @@ const deploy = async () => {
   console.log('Attempting to deploy from account', accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .deploy({ data: bytecode })
     .send({ gas: '1000000', from: accounts[0] });
-
+  console.log(interface);
   console.log('Contract deployed to', result.options.address);
 };
 deploy();
